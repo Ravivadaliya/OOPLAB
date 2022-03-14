e6bf87a515e8d6139ccbf81ea5f33e88be5ecc44
Ravi vadaliya 779 <88933926+Ravivadaliya@users.noreply.github.com>
2022-03-14 21:53:04 +0530

    Add files via upload

---
+++ practical 12/Second.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Set;
+
+import javax.security.auth.AuthPermission;
+import javax.xml.transform.Source;
+
+public class Second {
+    public static void main(String[] args) {
+
+        ArrayList<String> s1 = new ArrayList<>();
+
+        // enter name in arraylist
+        s1.add("Delhi");
+        s1.add("Mumbai");
+        s1.add("Banglore");
+        s1.add("Hydrabad");
+        s1.add("Ahmedabad");
+
+        System.out.println("Before change array list :");
+        for (String i : s1)
+            System.out.print(i + ",");
+
+        // change
+        s1.set(4, "surat");
+        System.out.println();
+        System.out.println();
+        System.out.println("After change array list :");
+        for (String i : s1)
+            System.out.print(i + ",");
+    }
+}
