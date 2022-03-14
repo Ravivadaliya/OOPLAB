e2d0f0b4981d1adc34b31573548d736acca89d25
Ravi vadaliya 779 <88933926+Ravivadaliya@users.noreply.github.com>
2022-03-14 21:57:06 +0530

    Update Second.java

--- practical 12/Second.java
+++ practical 12/Second.java
@@ -17,6 +17,7 @@ public class Second {
         s1.add("Hydrabad");
         s1.add("Ahmedabad");
 
+        //before change
         System.out.println("Before change array list :");
         for (String i : s1)
             System.out.print(i + ",");
@@ -25,6 +26,8 @@ public class Second {
         s1.set(4, "surat");
         System.out.println();
         System.out.println();
+        
+        //after change
         System.out.println("After change array list :");
         for (String i : s1)
             System.out.print(i + ",");
